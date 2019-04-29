0256829fc0f6ebba0660993f6e2562fea22bbab0
Andrew <ajalberd8@gmail.com>
2019-04-29 18:18:52 -0400

    added decoder and skeleton for bigcalc

--- decoder.vhdl
+++ decoder.vhdl
@@ -18,21 +18,31 @@ architecture behavioral of decoder is
 begin
     if (C and "11000000" == "11000000") then --DISP or COMP
         op <= "11";
-        dist <= C and "00100000";
-        r1 <= C and "00011000";
+        regw<="0"; --no writing to regs
+        imm<="0"; --no immediate ever
+        dist <= C(6);--and "00100000";
+        r1 <= C(4 downto 3);--and "00011000";
+        jcount <= C(0);
         if(C and "00100000" == "00000000") then --This is a COMP    
-            r2 <= C(2 downto 0); 
-            jcount <= C and "00000001" --FIX + REPLACE WITH THE DOWNTO TINGIES
+            r2 <= C(2 downto 0);
+        else
+            r2<="00" --default? 
         end if;
     else -- EVERYTHING ELSE
-        op <= C and "11000000";
+        op <= C(7 downto 6);-- and "11000000";
+        regw<= "1";
+        jcount<="0";
+        dist<="0";
         if(C and "11000000" == "00000000") then --LOAD
-            rd <= C and "00110000";
-            imm <= C and "00001111";
+            rd <= C(5 downto 4);--and "00110000";
+            imm <= C(3 downto 0);--and "00001111";
+            r1<="00";
+            r2<="00";
         else --EVERYTHING ELSE
-            rd <= C and "00110000";
-            r1 <= C and "00001100";
-            r2 <= C and "00000011";
+            rd <= C(5 downto 4);--and "00110000";
+            r1 <= C(3 downto 2);--and "00001100";
+            r2 <= C(1 downto 0); --and "00000011";
+            imm<="00";
         end if;
     end if;
 end behavioral;
