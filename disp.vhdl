20be79dedf6275b10e46559683f4c48475b05d51
ajalberd <ajalberd8@gmail.com>
2019-04-30 19:47:08 -0400

    Both disp.vhdl and disp_tb.vhdl compile but cannot make exec

--- disp.vhdl
+++ disp.vhdl
@@ -1,12 +1,12 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-library std;
-use std.textio.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity disp is 
     port(
         OP: in STD_LOGIC_VECTOR(1 downto 0);
         DIST: in STD_LOGIC;
+        CLK: in STD_LOGIC;
         RD1: in STD_LOGIC_VECTOR(7 downto 0)
     );
 end disp;
@@ -14,13 +14,14 @@ end disp;
 architecture behav of disp is
 
 begin 
-    process(OP, DIST) is
-        variable my_line : line;
+    process(OP, DIST, CLK) is
+        variable int_val : integer;
     begin
-    if((OP = "11") and (DIST = '1')) then
-        Hwrite(my_line, RD1);
-        writeline(OUTPUT, my_line);
-    else
+    if(rising_edge(CLK)) then
+        int_val := to_integer(signed(RD1));
+        if((OP = "11") and (DIST = '1')) then
+            report " " & integer'image(int_val) severity note;
+        end if;
     end if;
     end process;
 end behav;
--- disp_tb.vhdl
+++ disp_tb.vhdl
@@ -1,14 +1,16 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity dist_tb is
 end dist_tb;
 
 architecture behav of dist_tb is
 
-signal    OP: in STD_LOGIC_VECTOR(1 downto 0);
-signal    DIST: in STD_LOGIC;
-signal    RD1: in STD_LOGIC_VECTOR(7 downto 0);
+signal    OP:  STD_LOGIC_VECTOR(1 downto 0);
+signal    DIST: STD_LOGIC;
+signal    CLK: STD_LOGIC;
+signal    RD1: STD_LOGIC_VECTOR(7 downto 0);
 
 begin
 
@@ -17,17 +19,21 @@ type pattern_type is record
 
 OP:  STD_LOGIC_VECTOR(1 downto 0);
 DIST: STD_LOGIC;
+CLK: STD_LOGIC;
 RD1:  STD_LOGIC_VECTOR(7 downto 0);
 end record;
 
 type pattern_array is array (natural range <>) of pattern_type;
 constant patterns : pattern_array :=
-(("11",'1',"11111111")); --just print it out
+(("11",'0','0',"11111111"),
+("11",'1','1',"11111111"),
+("11",'1','0',"11111111")); --just print it out
 begin
 
 for n in patterns'range loop
 OP<=patterns(n).OP;
 DIST<=patterns(n).DIST;
+CLK<=patterns(n).CLK;
 RD1<=patterns(n).RD1;
 
 wait for 1 ns;
