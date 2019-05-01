5fab3d983dd1a9d622e509b6ecd20ca89b8dc2d1
Jared Glaser <jared@glaserfamily.net>
2019-04-30 20:35:14 -0400

    fixed jumpsel

--- jumpsel.vhdl
+++ jumpsel.vhdl
@@ -6,36 +6,36 @@ port(
         jcount: in std_logic;
         OP: in std_logic_vector(1 downto 0);
         DIST: in std_logic;
-        Feedback: in std_logic_vector(1 downto 0);
         clk: in std_logic;
         RD1, RD2: in std_logic_vector(7 downto 0);
-        O: in std_logic_vector(1 downto 0)
+        O: out std_logic_vector(1 downto 0)
 );
 end jumpsel;
 
 
 
-architecture behav of myregister is
+architecture behav of jumpsel is
     
-	signal value := "00":
-    
+	signal value: std_logic_vector(1 downto 0) := "00";
+    signal feedback: std_logic_vector(1 downto 0) := "00";
 
 begin
    
-process (clk)
+process (clk) 
 begin
     if(rising_edge(clk)) then
         O<= value;
+        feedback <= value;
     end if;
 end process;
 
-process(Jcount, OP, DIST, feedback, value, RD1, RD2)
+process(Jcount, OP, DIST, feedback, value, RD1, RD2) 
 begin
     if(feedback = "00") then --see if there is a comp instruction
-        if(jcount = '1') and (OP = "11") and (DIST = "0") and (RD1 = RD2) then --it is a comp with jump 2
+        if(jcount = '1') and (OP = "11") and (DIST = '0') and (RD1 = RD2) then --it is a comp with jump 2
             value<= "10";
         
-        elsif(jcount = '0') and (OP = "11") and (DIST = "0") and (RD1 = RD2) then --it is a comp with jump 1
+        elsif(jcount = '0') and (OP = "11") and (DIST = '0') and (RD1 = RD2) then --it is a comp with jump 1
             value<="01";
         
         else
@@ -45,7 +45,7 @@ begin
         if(value = "10") then
             value <= "01";
         elsif(value = "01") then
-            value <= "00"
+            value <= "00";
         end if;    
     end if;
 
