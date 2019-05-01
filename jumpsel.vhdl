1b9ebc8e4b5f788e0a6a4884d80ead859e567419
Jared Glaser <jared@glaserfamily.net>
2019-05-01 14:50:24 -0400

    fixed jump again

--- jumpsel.vhdl
+++ jumpsel.vhdl
@@ -16,37 +16,37 @@ end jumpsel;
 
 architecture behav of jumpsel is
     
-	signal value: std_logic_vector(1 downto 0) := "00";
-    signal feedback: std_logic_vector(1 downto 0) := "00";
+	signal feedback: std_logic_vector(1 downto 0) := "00";
+    signal value: std_logic_vector(1 downto 0) := "00";
 
 begin
    
 process (clk, value) 
 begin
    if(rising_edge(clk)) then
-    O<= feedback;
-    value <= feedback;
+    O<= value;
+    feedback <= value;
    end if;
 end process;
 
-process(Jcount, OP, DIST, value,RD1, RD2) 
+process(Jcount, OP, DIST, feedback,RD1, RD2) 
 begin
     --if(rising_edge(clk)) then
     if(feedback = "00") then --see if there is a comp instruction
         if(jcount = '1') and (OP = "11") and (DIST = '0') and (RD1 = RD2) then --it is a comp with jump 2
-            feedback<= "10";
+            value<= "10";
         
         elsif(jcount = '0') and (OP = "11") and (DIST = '0') and (RD1 = RD2) then --it is a comp with jump 1
-            feedback<="01";
+            value<="01";
         
         else
-            feedback<="00";
+            value<="00";
         end if;
-    else --decrement the value
+    else --decrement the feedback
         if(feedback = "10") then
-            feedback <= "01";
+            value <= "01";
         elsif(feedback = "01") then
-            feedback <= "00";
+            value <= "00";
         end if;    
     end if;
     
